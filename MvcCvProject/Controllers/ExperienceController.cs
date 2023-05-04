9b6254f242f9ccf34024171bacdb42f670eed2a5
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-05-04 16:28:07 +0300

    GenericRepository

---
+++ MvcCvProject/Controllers/ExperienceController.cs
@@ -0,0 +1,61 @@
+﻿using MvcCvProject.Models.Entity;
+using MvcCvProject.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Linq.Expressions;
+
+namespace MvcCvProject.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        EcperienceRepository repo = new EcperienceRepository();
+        public ActionResult Index()
+        {
+            var values = repo.List();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddExperience ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddExperience(TblExperience p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteExperience(int id)
+        {
+            TblExperience t = repo.Find(x=>x.ID == id);
+            repo.TDelete(t);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult UpdateExperience(int id)
+        {
+            TblExperience t = repo.Find(x => x.ID == id);
+            return View(t);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateExperience(TblExperience p)
+        {
+            TblExperience t = repo.Find(x => x.ID == p.ID);
+            t.Title = p.Title;
+            t.SecondTitle = p.SecondTitle;
+            t.Date = p.Date;
+            t.Description = p.Description;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCvProject/Controllers/SkillController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvProject.Models.Entity;
+using MvcCvProject.Repositories;
+
+namespace MvcCvProject.Controllers
+{
+    public class SkillController : Controller
+    {
+        GenericRepository<TblSkill> repo = new GenericRepository<TblSkill>();
+        public ActionResult Index()
+        {
+            var Skills = repo.List();
+            return View(Skills);
+        }
+
+        [HttpGet]
+        public ActionResult AddSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddSkill(TblSkill p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteSkill(int id)
+        {
+            var values = repo.Find(x => x.ID == id);
+            repo.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateSkill(int id)
+        {
+            var values = repo.Find(x => x.ID == id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateSkill(TblSkill p)
+        {
+            var values = repo.Find(x => x.ID == p.ID);
+            values.Skill = p.Skill;
+            values.Progress = p.Progress;
+            repo.TUpdate(values);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
