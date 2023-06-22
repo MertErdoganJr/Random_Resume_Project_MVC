9553e70d17d4de0b7d5a6636eb03cc5624fd760a
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-06-22 17:44:16 +0300

    Adminpage_edit

---
+++ MvcCvProject/Controllers/CertificateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvProject.Models.Entity;
+using MvcCvProject.Repositories;
+
+namespace MvcCvProject.Controllers
+{
+    public class CertificateController : Controller
+    {
+        // GET: Certificate
+        GenericRepository<TblCertificate> repo = new GenericRepository<TblCertificate>();
+        public ActionResult Index()
+        {
+            var certificate = repo.List();
+            return View(certificate);
+        }
+    }
+}
