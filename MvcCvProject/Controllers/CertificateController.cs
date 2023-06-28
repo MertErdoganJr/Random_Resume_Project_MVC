99064733a8ba7e2ae7b91aa7d77891d2a784ba77
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-06-28 18:24:41 +0300

    certificate_add_upadate

--- MvcCvProject/Controllers/CertificateController.cs
+++ MvcCvProject/Controllers/CertificateController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
 using MvcCvProject.Models.Entity;
@@ -16,6 +17,45 @@ namespace MvcCvProject.Controllers
         {
             var certificate = repo.List();
             return View(certificate);
+        }
+
+
+        [HttpGet]
+        public ActionResult GetCertificate(int id)
+        {
+            var certificate = repo.Find(x => x.ID == id);
+            ViewBag.d = id;
+            return View(certificate);
+        }
+
+        [HttpPost]
+        public ActionResult GetCertificate(TblCertificate t)
+        {
+            var certificate = repo.Find(x => x.ID == t.ID);
+            certificate.Description = t.Description;
+            certificate.Date = t.Date;
+            repo.TUpdate(certificate);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult AddCertificate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCertificate(TblCertificate p)
+        {
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteCertificate(int id)
+        {
+            var certificates = repo.Find(x => x.ID == id);
+            repo.TDelete(certificates);
+            return RedirectToAction("Index");
         }
     }
 }
